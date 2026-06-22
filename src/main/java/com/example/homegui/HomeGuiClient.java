4c92f13273ea776769e104a155760f54adfc340d
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-06-22 22:30:28 +0000

    Update HomeGuiClient.java

--- src/main/java/com/example/homegui/HomeGuiClient.java
+++ src/main/java/com/example/homegui/HomeGuiClient.java
@@ -6,21 +6,28 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.option.KeyBinding.Category;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HomeGuiClient implements ClientModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("homegui");
 
     private static KeyBinding openGuiKey;
 
     @Override
     public void onInitializeClient() {
 
+        Category category = KeyBinding.Category.create("category.homegui.main");
+
         openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.homegui.open_gui",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_H,
-                "category.homegui.main"
+                category
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
