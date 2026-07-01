1f49431fc4a8779384c7f8ad7f8010416090d12a
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-07-01 02:33:48 +0000

    Update HomeGuiClient.java

--- src/main/java/com/maxlananas/homegui/HomeGuiClient.java
+++ src/main/java/com/maxlananas/homegui/HomeGuiClient.java
@@ -7,6 +7,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.ResourceLocation;
 import com.mojang.blaze3d.platform.InputConstants;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
@@ -27,7 +28,7 @@ public class HomeGuiClient implements ClientModInitializer {
                 "key.homegui.open_gui",
                 InputConstants.Type.KEYSYM,
                 GLFW.GLFW_KEY_H,
-                new KeyMapping.Category("category.homegui.main")
+                new KeyMapping.Category(ResourceLocation.parse("homegui:category.main"))
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
