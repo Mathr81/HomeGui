ff0ea03fe07d53af451c2b4f4731705e971bc7c5
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-07-01 02:29:46 +0000

    Update HistoryScreen.java

--- src/main/java/com/maxlananas/homegui/screen/HistoryScreen.java
+++ src/main/java/com/maxlananas/homegui/screen/HistoryScreen.java
@@ -4,6 +4,8 @@ import com.maxlananas.homegui.HomesManager;
 import com.maxlananas.homegui.config.LangManager;
 import com.maxlananas.homegui.config.ModConfig;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.events.KeyEvent;
+import net.minecraft.client.gui.components.events.MouseButtonEvent;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
@@ -119,8 +121,8 @@ public class HistoryScreen extends Screen {
     }
 
     @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        int mx = (int) mouseX, my = (int) mouseY;
+    public boolean mouseClicked(MouseButtonEvent event, boolean doubleClick) {
+        int mx = (int) event.x(), my = (int) event.y();
         int panelX = width / 2 - 130;
         int panelW = 260;
         int panelY = 20;
@@ -153,17 +155,17 @@ public class HistoryScreen extends Screen {
             }
         }
 
-        return super.mouseClicked(mouseX, mouseY, button);
+        return super.mouseClicked(event, doubleClick);
     }
 
     @Override
-    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        if (keyCode == 256) {
+    public boolean keyPressed(KeyEvent event) {
+        if (event.key() == 256) {
             assert minecraft != null;
             minecraft.setScreen(parent);
             return true;
         }
-        return super.keyPressed(keyCode, scanCode, modifiers);
+        return super.keyPressed(event);
     }
 
     @Override
